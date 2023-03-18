06105d798b1cef17c9b94a19e722e20b2aae22ec
Olgica Djuric <olgica.djuric88@gmail.com>
2023-03-18 15:30:26 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:DotnetStarter.Logic.Tests/HelloWorldTest.cs

--- DotnetStarter.Logic.Tests/HelloWorldTest.cs
+++ DotnetStarter.Logic.Tests/HelloWorldTest.cs
@@ -5,6 +5,9 @@ namespace DotnetStarter.Logic.Tests
     public class HelloWorldTest
     {
         [Fact]
-        public void Hello_ReturnsWorld() => Assert.Equal("World!", HelloWorld.Hello());
+        public void Hello_ReturnsWorld()
+        {
+            Assert.Equal("World!", HelloWorld.Hello());
+        }
     }
 }
